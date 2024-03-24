a8fc58ff0dac0dead5c04407b20b79a10969e032
Daniel Kuhse <daniel.kuhse@tu-dortmund.de>
2024-03-24 10:06:07 +0000

    Modifications to make automatic testing easier

--- run_tracing.sh
+++ run_tracing.sh
@@ -3,16 +3,58 @@ mkdir -p traces
 # get the current time
 now=$(date +"%y_%m_%d__%H:%M:%S")
 
-# if a command line argument is given, use this as the session and path name, otherwise, use the current time
-if [ -z "$1" ]
+# Parse command-line options
+while getopts ":t:h" opt; do
+  case ${opt} in
+    t )
+      time=$OPTARG
+      ;;
+    h )
+      echo "Usage: run_tracing.sh [-t <time>]"
+      exit 0
+      ;;
+    \? )
+      echo "Invalid option: $OPTARG" 1>&2
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+# Parse positional arguments
+if [ $# -gt 0 ]; then
+    session_id=$1
+fi
+
+
+if [ -z "$session_id" ]
 then
-    session_name='session_'$now
+    echo "No session id provided, exiting"
+    exit 1
 else
-    session_name=session_$1
+    session_name='session_'$session_id
 fi
 
 # get the current path and append traces
 path=$(pwd)'/traces/'
 
-# start the tracing
-ros2 trace -p $path -s $session_name
+if [ -z "$time" ]
+then
+    # just start the tracing
+    ros2 trace -p $path -s $session_name
+else    
+    # execute above expect script
+    expect -c "
+      spawn ros2 trace -p $path -s $session_name
+      expect \"press enter to start...\"
+      send \"\r\"
+      puts \"\nStarting tracing...\"
+      sleep $time
+      expect \"press enter to stop...\"
+      send \"\r\"
+      interact
+    "
+    echo "Tracing stopped"
+fi
---
+++ test_and_trace.sh
@@ -0,0 +1,53 @@
+# runs run_tracing.sh -t $time $session_id
+# then executes ros2 run $package $executable
+# terminate the process after the given time
+
+# parse command-line options (-t, -p, -e)
+while getopts ":t:p:e:h" opt; do
+    case ${opt} in
+        t )
+            time=$OPTARG
+            ;;
+        p )
+            package=$OPTARG
+            ;;
+        e )
+            executable=$OPTARG
+            ;;
+        h )
+            echo "Usage: test_and_trace.sh [-t <time>] [-p <package>] [-e <executable>] [-h]"
+            echo "Options:"
+            echo "  -t <time>        Specify the time to run the tracing"
+            echo "  -p <package>     Specify the package to run"
+            echo "  -e <executable>  Specify the executable to run"
+            echo "  -h               Display this help message"
+            exit 0
+            ;;
+        \? )
+            echo "Invalid option: $OPTARG" 1>&2
+            ;;
+        : )
+            echo "Invalid option: $OPTARG requires an argument" 1>&2
+            ;;
+    esac
+done
+
+# shift the options so that the positional arguments can be parsed
+shift $((OPTIND -1))
+
+# parse positional arguments
+if [ $# -gt 0 ]; then
+    session_id=$1
+fi
+
+echo "Running the package $package with the executable $executable for $time seconds, session id: $session_id"
+
+# execute run_tracing.sh -t $time $session_id, send enter key to the process, sleep for 5 seconds, send enter key to the process
+source ./run_tracing.sh -t $time $session_id &
+
+source ./packages/install/setup.sh
+
+sleep 3
+
+# execute the package and executable, suppress output, terminate the process after the given time
+timeout $time ros2 run $package $executable
